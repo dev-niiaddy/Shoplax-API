bebb76eb6684b5b35dee97518f8e88d0b0bdf826
Addy Godwin Nii <dev.niiaddy@gmail.com>
2019-04-11 01:17:05 +0000

    update

--- dist/models/order.js
+++ dist/models/order.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Types = mongoose.Types;
+const Types = mongoose.Schema.Types;
 const Order = new Schema({
     products: {
-        type: Types.Array
+        type: Types.Array,
     },
+    address: {}
 });
 //# sourceMappingURL=order.js.map
--- lib/models/order.ts
+++ lib/models/order.ts
@@ -2,11 +2,13 @@ import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const Types = mongoose.Types;
+const Types = mongoose.Schema.Types;
 
 const Order = new Schema({
     products: {
-        type: Types.Array
+        type: Types.Array,
     },
-    
+    address: {
+        
+    }
 });
